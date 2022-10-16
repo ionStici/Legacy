9209c87675ef7a626b8796d77f0e21a928b64de0
Ion Stici <ionsticidev@gmail.com>
2022-10-16 14:54:36 +0300

    Fix responsive copyright note but

--- scripts/copyButton.js
+++ scripts/copyButton.js
@@ -50,3 +50,27 @@ copyrightBtn.addEventListener("click", function (e) {
 });
 
 // // // // // // // // // // // // // // // // // // // //
+
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia("(min-width: 75em)");
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    // Then log the following message to the console
+
+    document.querySelector(".nav__note").style.opacity = 1;
+    document.querySelector(".nav__note").style.display = "block";
+  } else {
+    document.querySelector(".nav__note").style.opacity = 0;
+    document.querySelector(".nav__note").style.display = "none";
+  }
+}
+
+// Register event listener
+mediaQuery.addListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
+
+// // // // // // // // // // // // // // // // // // // //
