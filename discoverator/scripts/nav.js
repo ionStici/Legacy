06fb68f5e48197e8a981e91cd597a2b8da7007cd
Ion Stici <ionsticidev@gmail.com>
2022-10-25 15:03:43 +0300

    Discov: use event handler for nav buttons, remove event propagation

--- discoverator/scripts/nav.js
+++ discoverator/scripts/nav.js
@@ -5,6 +5,8 @@ const btnsBox = document.querySelector(".nav__lists");
 const btns = document.querySelectorAll(".nav__list");
 const layouts = document.querySelectorAll(".content");
 
+/*
+// EVENT PROPAGATION
 btnsBox.addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -38,6 +40,79 @@ btnsBox.addEventListener("click", function (e) {
     //   const clicker = new Clicker();
     // }
   }
+});
+*/
+
+// // // // // // // // // // // // // // // // // // // //
+
+btns[0].addEventListener("click", function (e) {
+  e.preventDefault();
+
+  btns.forEach((btn) => {
+    btn.classList.remove("nav__list--active");
+  });
+
+  layouts.forEach((layout) => {
+    layout.style.display = "none";
+  });
+
+  this.classList.add("nav__list--active");
+
+  document.querySelector(".content__home").style.display = "flex";
+});
+
+// // // // // // // // // // // // // // // // // // // //
+
+btns[1].addEventListener("click", function (e) {
+  e.preventDefault();
+
+  btns.forEach((btn) => {
+    btn.classList.remove("nav__list--active");
+  });
+
+  layouts.forEach((layout) => {
+    layout.style.display = "none";
+  });
+
+  this.classList.add("nav__list--active");
+
+  document.querySelector(".clicker").style.display = "flex";
+});
+
+// // // // // // // // // // // // // // // // // // // //
+
+btns[2].addEventListener("click", function (e) {
+  e.preventDefault();
+
+  btns.forEach((btn) => {
+    btn.classList.remove("nav__list--active");
+  });
+
+  layouts.forEach((layout) => {
+    layout.style.display = "none";
+  });
+
+  this.classList.add("nav__list--active");
+
+  document.querySelector(".locations").style.display = "block";
+});
+
+// // // // // // // // // // // // // // // // // // // //
+
+btns[3].addEventListener("click", function (e) {
+  e.preventDefault();
+
+  btns.forEach((btn) => {
+    btn.classList.remove("nav__list--active");
+  });
+
+  layouts.forEach((layout) => {
+    layout.style.display = "none";
+  });
+
+  this.classList.add("nav__list--active");
+
+  document.querySelector(".game").style.display = "flex";
 });
 
 // // // // // // // // // // // // // // // // // // // //
