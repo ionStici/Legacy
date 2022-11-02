03ea814e0a14755699b59b0b89f2cdcb818017d9
Ion Stici <ionsticidev@gmail.com>
2022-11-02 14:20:05 +0200

    Traviavel: Implement responsive design > 901

--- projects/traviavel/js/script.js
+++ projects/traviavel/js/script.js
@@ -2,16 +2,18 @@
 
 const navCheckbox = document.querySelector('.nav__checkbox');
 
+// prettier-ignore
 const checking = function () {
     if (navCheckbox.checked === true) {
         document.querySelector('.nav__ul').style.display = 'none';
         document.querySelector('.nav__ul').style.opacity = '0';
+        document.querySelector('body').style.overflow = 'revert';
     }
 
     if (navCheckbox.checked === false) {
         document.querySelector('.nav__ul').style.display = 'block';
-        // prettier-ignore
         setTimeout(() => document.querySelector('.nav__ul').style.opacity = '1', 400);
+        document.querySelector('body').style.overflow = 'hidden';
     }
 };
 
@@ -25,6 +27,7 @@ document.querySelector('.nav__background').addEventListener('click', function ()
     navCheckbox.checked = false;
     document.querySelector('.nav__ul').style.display = 'none';
     document.querySelector('.nav__ul').style.opacity = '0';
+    document.querySelector('body').style.overflow = 'revert';
 });
 
 // // // // // // // // // // // // // // // // // // // // // // //
