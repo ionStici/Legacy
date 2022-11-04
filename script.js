247554c4dadbacf011a78a90b3c306701885c316
Ion Stici <ionsticidev@gmail.com>
2022-11-04 18:23:02 +0200

    Viatour: Add hotel description component

--- script.js
+++ script.js
@@ -1,46 +1,51 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+
 const insertMarkup = function () {
   const markup = `
-    <main class="main">
-        <div class="links">
-            <a class="link" href="./p/viatour">Viatour</a>
-            <a class="link" href="./p/traviavel">Traviavel</a>
-            <a class="link" href="./p/slider">Slider</a>
-            <a class="link" href="./p/discoverator">Discoverator</a>
-        </div>
         <div class="links">
             <a class="link" href="./p/irina">Irina</a>
             <a class="link" href="./p/ionsticidev">ionstici.dev</a>
         </div>
-    </main>
     `;
 
-  document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
+  document.querySelector(".main").insertAdjacentHTML("beforeend", markup);
 };
 
-const input = document.querySelector(".input");
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
 
-document.querySelector(".form").addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const pin = 11;
-  const inputPin = +input.value;
+  const pin = 111;
+  const inputPin = input.value;
 
-  if (!inputPin) {
+  if (inputPin.length === 0) {
     input.placeholder = "Empty Input";
     document.documentElement.style.setProperty("--placeholder-color", "red");
     return;
   }
 
-  if (pin !== inputPin) {
+  if (pin !== +inputPin) {
     input.placeholder = "Wrong Pin";
     document.documentElement.style.setProperty("--placeholder-color", "red");
   }
 
-  if (pin === inputPin) {
-    document.querySelector("body").innerHTML = "";
-    insertMarkup();
+  if (pin === +inputPin) {
+    input.placeholder = "Correct";
+    document.documentElement.style.setProperty("--placeholder-color", "green");
+    setTimeout(() => {
+      form.remove();
+      insertMarkup();
+    }, 500);
   }
 
   input.value = "";
   input.blur();
 });
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
